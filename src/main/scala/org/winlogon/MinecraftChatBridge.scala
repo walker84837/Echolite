df40a7208f276ecbb5f93d331d1fe27f47a949fe
winlogon <walker84837@gmail.com>
2025-01-28 01:39:08 +0100

    feat: add player leave event

--- src/main/scala/org/winlogon/MinecraftChatBridge.scala
+++ src/main/scala/org/winlogon/MinecraftChatBridge.scala
@@ -1,26 +1,46 @@
 package org.winlogon
 
+import org.bukkit.event.player.PlayerQuitEvent.QuitReason
+import org.bukkit.event.player.{AsyncPlayerChatEvent, PlayerJoinEvent, PlayerQuitEvent}
 import org.bukkit.event.{EventHandler, Listener}
-import org.bukkit.event.player.{AsyncPlayerChatEvent, PlayerJoinEvent}
 
 class MinecraftChatBridge(config: Configuration, discordBotManager: DiscordBotManager) extends Listener {
-
   @EventHandler
   def onPlayerChat(event: AsyncPlayerChatEvent): Unit = {
-    // TODO: remove minimessage syntax when sending to discord
     val playerMessage = "&[a-zA-Z0-9]".r replaceAllIn (event.getMessage, "")
+    val miniMessageFormat = """<[^>]*>""".r replaceAllIn (playerMessage, "")
     val message = config.minecraftMessage
       .replace("$user_name", event.getPlayer.getName)
-      .replace("$message", playerMessage.trim)
+      .replace("$message", miniMessageFormat.trim)
     discordBotManager.sendMessageToDiscord(message)
   }
 
   @EventHandler
   def onPlayerJoin(event: PlayerJoinEvent): Unit = {
-      if (config.sendPlayerJoinMessages) {
+    if (config.sendPlayerJoinMessages) {
         val player = event.getPlayer()
         discordBotManager.sendMessageToDiscord(s"**${player.getName}** has joined the server!")
-      }
     }
-}
+  }
 
+  @EventHandler
+  def onPlayerQuit(event: PlayerQuitEvent): Unit = {
+    if (!config.sendPlayerJoinMessages) {
+      return
+    }
+
+    val quitReason = event.getReason()
+    val player = event.getPlayer()
+    val initialString = s"**${player.getName}**"
+    quitReason match {
+      case QuitReason.DISCONNECTED => discordBotManager.sendMessageToDiscord(s"$initialString has left the server!")
+      case QuitReason.TIMED_OUT | QuitReason.ERRONEOUS_STATE => {
+        discordBotManager.sendMessageToDiscord(
+          s"$initialString has been has been disconnected due to a connection timeout or an unexpected error."
+        )
+      }
+      case QuitReason.KICKED => discordBotManager.sendMessageToDiscord(s"$initialString has been kicked.")
+      case null => discordBotManager.sendMessageToDiscord(s"$initialString has been kicked for an unknown reason.")
+    }
+  }
+}
